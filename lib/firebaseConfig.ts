f9d44d4237055c814484727c9744d1d119e3509b
Krishna Mohan <krishanmohank974@gmail.com>
2024-12-12 16:28:01 +0530

    bug fixed api changed

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -3,18 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: (process.env.NEXT_PUBLIC_FIREBASE_API_KEY =
-    "AIzaSyDsePCy_62pBWMlKoLViZjX4PjZ1FndYKg"),
-  authDomain: (process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN =
-    "rakshak-sih-3f4db.firebaseapp.com"),
-  projectId: (process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID =
-    "rakshak-sih-3f4db"),
-  storageBucket: (process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET =
-    "rakshak-sih-3f4db.firebasestorage.app"),
-  messagingSenderId: (process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID =
-    "571556329211"),
-  appId: (process.env.NEXT_PUBLIC_FIREBASE_APP_ID =
-    "1:571556329211:web:d5cd9f830442272094a25e"),
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
